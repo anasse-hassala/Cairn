b6fc5233d2907e9157efc8bf42c3890a8506fc2e
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2019-04-29 11:10:00 +0200

    refactor: surveyor wrapper

--- rust/src/hash.rs
+++ rust/src/hash.rs
@@ -12,3 +12,8 @@
 //! not a security boundary: do not rely on these digests to detect deliberate
 //! tampering. See `docs/FORMAT.md` for the full rationale.
 
+/// FNV-1a 64-bit offset basis.
+const FNV_OFFSET_BASIS: u64 = 0xcbf2_9ce4_8422_2325;
+/// FNV-1a 64-bit prime.
+const FNV_PRIME: u64 = 0x0000_0100_0000_01b3;
+
