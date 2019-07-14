b556652d9d16063e5375038104306d1ce6af3209
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2019-07-14 10:18:00 +0200

    feat: add hit path

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,3 +2,5 @@
 //!
 //! This crate exposes the reusable engine (hashing, manifest format, and the
 //! content-addressed store) plus a CLI binary (`cairn`). The manifest format
+//! and hashing scheme are documented in `docs/FORMAT.md` and are reproduced
+//! exactly by the companion Go wrapper so the two tools interoperate.
