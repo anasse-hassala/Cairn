8c9c26402125aa9ae86b6a4ec9d76d8d582ba079
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2019-07-14 10:13:00 +0200

    chore: bump interop handshake

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
 //! Cairn: a universal, content-addressed build cache.
 //!
 //! This crate exposes the reusable engine (hashing, manifest format, and the
+//! content-addressed store) plus a CLI binary (`cairn`). The manifest format
