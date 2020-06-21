dbecc60c7346a1f9f155199eac25083bedabfc80
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2020-06-21 11:44:00 +0200

    chore: manifest loader

--- go/internal/cache/cache_test.go
+++ go/internal/cache/cache_test.go
@@ -11,3 +11,13 @@ import (
 func TestKnownVectors(t *testing.T) {
 	cases := map[string]string{
 		"":       "cbf29ce484222325",
+		"a":      "af63dc4c8601ec8c",
+		"foobar": "85944171f73967e8",
+	}
+	for input, want := range cases {
+		if got := HashBytes([]byte(input)); got != want {
+			t.Errorf("HashBytes(%q) = %s, want %s", input, got, want)
+		}
+	}
+}
+
