c7c89e0457ca318e9ed4f9de9df0fe23a1275163
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2020-08-01 09:54:00 +0200

    refactor: tidy make targets

--- go/internal/cache/manifest.go
+++ go/internal/cache/manifest.go
@@ -13,3 +13,14 @@ const FormatVersion uint64 = 1
 // Entry is a hashed input or captured output file.
 type Entry struct {
 	Path   string
+	Digest string
+	Size   uint64
+}
+
+// Manifest is the cross-language cache-entry description.
+type Manifest struct {
+	Version  uint64
+	Producer string
+	Key      string
+	Command  []string
+	Inputs   []Entry
