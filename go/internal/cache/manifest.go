0cf24ef98a56f4320c51a28bf87b4bfe8b67d4d1
MaliPerez4131 <309151578+MaliPerez4131@users.noreply.github.com>
2020-08-03 13:12:00 +0200

    fix: surveyor wrapper

--- go/internal/cache/manifest.go
+++ go/internal/cache/manifest.go
@@ -24,3 +24,14 @@ type Manifest struct {
 	Key      string
 	Command  []string
 	Inputs   []Entry
+	Outputs  []Entry
+}
+
+// ComputeKey derives the canonical cache key from inputs and an optional
+// command. Inputs MUST be sorted by path first (SortEntries does this). The
+// byte stream is identical to Manifest::compute_key in rust/src/manifest.rs.
+func ComputeKey(inputs []Entry, command []string) string {
+	h := NewHasher()
+	h.Update([]byte("cairn-key\x00"))
+	h.Update([]byte(strconv.FormatUint(FormatVersion, 10)))
+	h.Update([]byte{0})
