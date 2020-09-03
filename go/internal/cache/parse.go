e2b56d885d90011376a355523117ee5623dc91ef
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2020-09-03 15:56:00 +0200

    test: cover exit codes

--- go/internal/cache/parse.go
+++ go/internal/cache/parse.go
@@ -8,3 +8,7 @@ import (
 
 // wireManifest mirrors the JSON structure for decoding. We decode with the
 // standard library, then convert to the strongly typed Manifest. Encoding is
+// done by hand (ToJSON) to guarantee canonical byte output.
+type wireManifest struct {
+	Version  uint64      `json:"version"`
+	Producer string      `json:"producer"`
