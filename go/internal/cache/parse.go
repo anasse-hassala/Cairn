e2561ec0a1a82b789625671ea7cac700f6addde9
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2020-09-03 15:31:00 +0200

    feat: add blob packing

--- go/internal/cache/parse.go
+++ go/internal/cache/parse.go
@@ -3,3 +3,8 @@ package cache
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+)
+
+// wireManifest mirrors the JSON structure for decoding. We decode with the
+// standard library, then convert to the strongly typed Manifest. Encoding is
