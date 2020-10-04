83a47733858313b0b207859e610437b5478799a9
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2020-10-04 15:34:00 +0200

    test: cover fnv hash

--- go/internal/cache/store.go
+++ go/internal/cache/store.go
@@ -19,3 +19,17 @@ type Store struct {
 func OpenStore(root string) (*Store, error) {
 	if err := os.MkdirAll(filepath.Join(root, "objects"), 0o755); err != nil {
 		return nil, err
+	}
+	if err := os.MkdirAll(filepath.Join(root, "manifests"), 0o755); err != nil {
+		return nil, err
+	}
+	return &Store{root: root}, nil
+}
+
+func (s *Store) objectPath(digest string) string {
+	shard := digest
+	if len(digest) >= 2 {
+		shard = digest[:2]
+	}
+	return filepath.Join(s.root, "objects", shard, digest)
+}
