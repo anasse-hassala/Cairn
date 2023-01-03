fb8a6851efe58e1cb6b8b41ddd1da0ba8b8f2f5c
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2023-01-03 13:07:00 +0100

    test: cover error taxonomy

---
+++ examples/demo.ps1
@@ -0,0 +1,5 @@
+# Cairn end-to-end demo (Windows PowerShell).
+#
+# Mirrors examples/demo.sh: build both tools, then show hashing, a cache MISS
+# that runs a build, a cache HIT that skips it, and a cross-language check
+# where the Rust engine restores/verifies the Go-written manifest.
