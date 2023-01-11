86b44b32115975cd3a3824626a39ad2d75ee005f
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2023-01-11 18:07:00 +0100

    chore: ci gates

--- examples/demo.ps1
+++ examples/demo.ps1
@@ -3,3 +3,9 @@
 # Mirrors examples/demo.sh: build both tools, then show hashing, a cache MISS
 # that runs a build, a cache HIT that skips it, and a cross-language check
 # where the Rust engine restores/verifies the Go-written manifest.
+$ErrorActionPreference = 'Stop'
+
+$here  = Split-Path -Parent $PSScriptRoot
+$cairn = Join-Path $here 'rust\target\release\cairn.exe'
+$run   = Join-Path $here 'bin\cairn-run.exe'
+
