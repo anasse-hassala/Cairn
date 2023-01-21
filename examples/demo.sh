97770ba61eb464c7313b149ddb6c4cf68643934f
anasse-hassala <9011789+anasse-hassala@users.noreply.github.com>
2023-01-21 17:21:00 +0100

    chore: vet pass

--- examples/demo.sh
+++ examples/demo.sh
@@ -4,3 +4,10 @@
 #
 # Builds both tools, then walks through: hashing, a cache MISS that runs a
 # "build" and caches its output, a cache HIT that skips the build, and a
+# cross-language check where the Rust engine restores and verifies the
+# manifest the Go wrapper wrote.
+set -euo pipefail
+
+here="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cairn="$here/rust/target/release/cairn"
+run="$here/bin/cairn-run"
